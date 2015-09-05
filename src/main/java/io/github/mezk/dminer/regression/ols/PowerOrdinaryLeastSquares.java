3a162301ff9101dfc79e3511b7a45cbfb4a184e5
Andrei Selkin <andreyselkin@gmail.com>
2015-09-05 23:55:44 +0300

    Issue #2: Implemented PowerOrdinaryLeastSquares.

--- src/main/java/io/github/mezk/dminer/regression/ols/PowerOrdinaryLeastSquares.java
+++ src/main/java/io/github/mezk/dminer/regression/ols/PowerOrdinaryLeastSquares.java
@@ -8,9 +8,37 @@ package io.github.mezk.dminer.regression.ols;
 public class PowerOrdinaryLeastSquares extends LinearOrdinaryLeastSquares {
 
     @Override
-    public Result process(double[][] inputData) {
+    public  strictfp Result process(double[][] inputData) {
 
-        //TODO: http://works.tarefer.ru/50/100013/index.html#
-        return super.process(inputData);
+        final double[][] convertedInputData = new double[inputData[0].length][inputData[1].length];
+        convertedInputData[0] = lnArray(inputData[0]);
+        convertedInputData[1] = lnArray(inputData[1]);
+
+        final Result linearOslResult = super.process(convertedInputData);
+        final double a = linearOslResult.getCoefficientA();
+        final double convertedB = linearOslResult.getCoefficientB();
+        final double b = StrictMath.exp(convertedB);
+
+        //TODO: Calculate correlation coefficient.
+
+        final Result result = new Result();
+        result.setCoefficientA(a);
+        result.setCoefficientB(b);
+        return result;
+    }
+
+    /**
+     * Returns an array each element of which is a natural logarithm
+     * of the original array.
+     * @param array array.
+     * @return array each element of which is a natural logarithm
+     * of the original array.
+     */
+    public static double[] lnArray(double[] array) {
+        final double[] result = new double[array.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = StrictMath.log(array[i]);
+        }
+        return result;
     }
 }
---
+++ src/test/java/io/github/mezk/dminer/regression/ols/PowerOrdinaryLeastSquaresTest.java
@@ -0,0 +1,20 @@
+package io.github.mezk.dminer.regression.ols;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PowerOrdinaryLeastSquaresTest {
+    @Test
+    public void testProcess() {
+        // y = 2x^2
+        double[][] inputData = {
+            {1, 2, 3, 4, 5, 6},
+            {2, 8, 18, 32, 50, 72},
+        };
+        final AbstractOrdinaryLeastSquares osl = new PowerOrdinaryLeastSquares();
+        final Result result = osl.process(inputData);
+
+        Assert.assertEquals(2.0, result.getCoefficientA(), 0.0000000000000001);
+        Assert.assertEquals(2.0, result.getCoefficientB(), 0.0000000000000001);
+    }
+}
