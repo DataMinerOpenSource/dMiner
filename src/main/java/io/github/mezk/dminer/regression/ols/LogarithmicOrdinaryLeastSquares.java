11dfa2417e586b16ba593b7a6d9b517cd0edb44b
Andrei Selkin <andreyselkin@gmail.com>
2015-09-10 09:55:43 +0300

    Issue #3: Implemented LogarithmicOrdinaryLeastSquares.

--- src/main/java/io/github/mezk/dminer/regression/ols/LogarithmicOrdinaryLeastSquares.java
+++ src/main/java/io/github/mezk/dminer/regression/ols/LogarithmicOrdinaryLeastSquares.java
@@ -1,5 +1,7 @@
 package io.github.mezk.dminer.regression.ols;
 
+import edu.stanford.nlp.math.ArrayMath;
+
 /**
  * Ordinary Least Squares method for logarithmic function.
  *
@@ -9,7 +11,19 @@ public class LogarithmicOrdinaryLeastSquares extends LinearOrdinaryLeastSquares 
 
     @Override
     public Result process(double[][] inputData) {
-        //TODO: http://works.tarefer.ru/50/100013/index.html#
-        return super.process(inputData);
+        final double[][] convertedInputData = new double[inputData[0].length][inputData[1].length];
+        convertedInputData[0] = ArrayMath.log(inputData[0]);
+        convertedInputData[1] = inputData[1];
+
+        final Result linearOslResult = super.process(convertedInputData);
+        final double a = linearOslResult.getCoefficientA();
+        final double b = linearOslResult.getCoefficientB();
+
+        //TODO: Calculate correlation coefficient.
+
+        final Result result = new Result();
+        result.setCoefficientA(a);
+        result.setCoefficientB(b);
+        return result;
     }
 }
