b6800a24d249434195e35bf84e3d48479a0e4582
Vladislav Lisetskiy <vladlis54@gmail.com>
2015-09-14 22:19:34 +0300

    Issue #4: Implement OLS for hyperbolic function

--- src/main/java/io/github/mezk/dminer/regression/osl/HyperbolicOrdinaryLeastSquares.java
+++ src/main/java/io/github/mezk/dminer/regression/osl/HyperbolicOrdinaryLeastSquares.java
@@ -1,16 +1,37 @@
 package io.github.mezk.dminer.regression.osl;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import io.github.mezk.dminer.utils.ArrayMath;
+
 /**
  * Ordinary Least Squares method for hyperbolic function.
  *
  * @author Andrei Selkin
+ * @author Vladislav Lisetskiy
  */
 public class HyperbolicOrdinaryLeastSquares extends LinearOrdinaryLeastSquares {
 
     @Override
-    public Result process(double[][] inputData) {
+    public strictfp Result process(double[][] inputData) throws IllegalArgumentException {
 
-        //TODO: http://works.tarefer.ru/50/100013/index.html#
-        return super.process(inputData);
+        if (ArrayUtils.contains(inputData[0], 0.0)) {
+            throw new IllegalArgumentException("x cannot be zero in hyperbolic function.");
+        }
+
+        final double[][] convertedInputData = new double[inputData[0].length][inputData[1].length];
+        convertedInputData[0] = ArrayMath.pow(inputData[0], -1);
+        convertedInputData[1] = inputData[1];
+
+        final Result linearOslResult = super.process(convertedInputData);
+        final double a = linearOslResult.getCoefficientA();
+        final double b = linearOslResult.getCoefficientB();
+
+        //TODO: Calculate correlation coefficient.
+
+        final Result result = new Result();
+        result.setCoefficientA(a);
+        result.setCoefficientB(b);
+        return result;
     }
 }
